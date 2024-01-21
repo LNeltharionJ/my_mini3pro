aa2018d3111799e0391145d6bb68495393817067
LNeltharionJ <1392509372@qq.com>
2024-01-21 17:07:58 +0800

    first time for 4 schools

---
+++ 备份（无音乐）/photo_try.js
@@ -0,0 +1,222 @@
+const photosInfo = [
+    {
+        cephalogram_url: './asset/img/CUG/CUG_1.jpg',
+        name: 'China University of Geosciences, Wuhan',
+        introduction: '中国地质大学（武汉）-艰苦朴素 求真务实',
+        size: 'width-11',
+        music_url: './asset/music/ddd.mp3',
+        children: [
+            {
+                children_url: './asset/img/CUG/CUG_2.jpg',
+                children_introduction:'该怎么形容？它如同一本永恒的诗集,在四季轮转之间展现出不同的容颜，却又保留着那份不变的风采',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_3.jpg',
+                children_introduction: '',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_4.jpg',
+                children_introduction: '春抚羞花，夏展生机，秋舞金黄余音，冬着银装素裹',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_5.jpg',
+                children_introduction: '',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_6.jpg',
+                children_introduction: '四季轮转间，它展现出不同的容颜，却始终保留着那份不变的风采',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_7.jpg',
+                children_introduction: '',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_8.jpg',
+                children_introduction: '一曲石阶飘玉壶 半峰微垂碧潭青 若非群翠枝头现 笑道空逐水中亭',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_9.jpg',
+                children_introduction: '',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_1.jpg',
+                children_introduction: '这里，送你来自北区的微笑',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_11.jpg',
+                children_introduction: '',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_12.jpg',
+                children_introduction: '',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_13.jpg',
+                children_introduction: '猜猜这是哪？',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_14.jpg',
+                children_introduction: '',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_15.jpg',
+                children_introduction: '化石？人造湿地？新开的校史馆？今年进了不少人呢',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_16.jpg',
+                children_introduction: '',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/CUG/CUG_17.jpg',
+                children_introduction: '黑天鹅？鸭子？大鹅？鸿雁？鹅蛋？？？',
+                children_size:'width-11',
+            },
+
+        ]
+    },
+    {
+        cephalogram_url: './asset/img/WHU/WHU_2.0.jpg',
+        name: 'Wuhan University',
+        introduction: '武汉大学-自强 弘毅 求是 拓新',
+        size: 'width-11',
+        music_url: './asset/music/当你-王心凌.mp3',
+        children: [
+            {
+                children_url: './asset/img/WHU/WHU_2.9.jpg',
+                children_introduction: '',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_2.5.jpg',
+                children_introduction: '光芒万丈！！！',
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_1.jpg',
+                children_introduction: `迷路了，没找到在哪，不过在天上找到了`,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_3.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_4.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_5.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_6.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_7.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_8.jpg',
+                children_introduction: `我不喜欢绿色，但你是例外`,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_9.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_10.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_11.jpg',
+                children_introduction: `有位领导说，跑起来就有生气了`,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_12.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_13.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_14.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_15.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_16.jpg',
+                children_introduction: `四年前我就发现，你似乎很擅长把夹杂生死的竞争演绎成凝脂般的永恒`,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_17.jpg',
+                children_introduction: `130周年的校庆和拍摄现场？我能不能回来凑个热闹？`,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_18.jpg',
+                children_introduction: `哦~美妙的现代建筑的气息和突兀的紫色电瓶车~`,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_19.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_20.jpg',
+                children_introduction: `所以，红色的小狐狸为什么要躲着我呢？`,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_21.jpg',
+                children_introduction: ``,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_22.jpg',
+                children_introduction: `你在闻看不见的蔷薇吗，小猫咪？`,
+                children_size:'width-11',
+            },
+            {
+                children_url: './asset/img/WHU/WHU_23.jpg',
+                children_introduction: `有一瞬间以为是左边那个大学`,
+                children_size:'width-11',
+            },
+        ]
+    },
+
+]
